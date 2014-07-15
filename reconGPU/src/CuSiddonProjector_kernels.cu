d440518f3d45b6ca5761a98ca2fb9ac86c17b31d
belzunce <belzunce@4aa1ec02-d693-4363-b663-c1bee9069998>
2014-07-15 10:45:10 +0000

    Subo archivo donde van los kernels.
    git-svn-id: https://svn.code.sf.net/p/apirl/code/trunk@6 4aa1ec02-d693-4363-b663-c1bee9069998

---
+++ reconGPU/src/CuSiddonProjector_kernels.cu
@@ -0,0 +1,55 @@
+/**
+	\file CuSiddonProjector_kernels.cu
+	\brief Archivo que contiene los kernels utilizados en la clase CuSiddonProjector.
+	
+	\todo 
+	\bug
+	\warning
+	\author Martín Belzunce (martin.a.belzunce@gmail.com)
+	\date 2014.07.11
+	\version 1.1.0
+*/
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#define MAX_PHI_VALUES	512	// Máxima cantidad de valores en el angulo theta que puede admitir la implementación.
+#define MAX_R_VALUES	512	// Idem para R.
+#define MAX_Z_VALUES	92	// Idem para anillos (z)
+#define MAX_SPAN	7	// Máximo valor de combinación de anillos por sinograma 2D.
+
+// Memoria constante con los valores de los angulos de la proyeccion,
+__device__ __constant__ float cuda_values_theta[MAX_PHI_VALUES];
+
+// Memoria constante con los valores de la distancia r.
+__device__ __constant__ float cuda_values_r[MAX_R_VALUES];
+
+// Memoria constante con los valores de la coordenada axial o z.
+__device__ __constant__ float cuda_values_z[MAX_Z_VALUES];
+
+// Memoria constante con los valores de la coordenada axial o z.
+__device__ __constant__ float cuda_values_z[MAX_Z_VALUES*MAX_Z_VALUES][MAX_SPAN];
+	
+__global__ void CUDA_Forward_Projection (float* volume, float* michelogram, float* michelogram_measured, int numR, int numProj, int)
+{
+  int indexSino2D =  threadIdx.x + (blockIdx.x * cuda_threads_per_block);
+  if(indexSino2D>=cudaBinsSino2D)
+    return;
+  int iR = indexSino2D % cuda_michelogram_size.NR;
+  int iProj = (int)((float)indexSino2D / (float)cuda_michelogram_size.NR);
+  int iZ = blockIdx.y;	// Block index y, index of the 2d sinogram, from which it can be taken  position Z1 and Z2 of th sinogram2d
+  int indexRing1 = iZ%cuda_michelogram_size.NZ; //Ring 1 : Las columnas;
+  int indexRing2 = (int)(iZ/cuda_michelogram_size.NZ);	// Ring 2 las filas	
+  float4 P1;// = make_float4(0,0,0);
+  float4 P2;
+  float4 LOR;
+  int indiceMichelogram = iR + iProj * cuda_michelogram_size.NR
+	  + iZ * (cuda_michelogram_size.NProj * cuda_michelogram_size.NR);
+  if(michelogram_measured[indiceMichelogram] != 0)
+  {
+    CUDA_GetPointsFromLOR(cuda_values_phi[iProj], cuda_values_r[iR], cuda_values_z[indexRing1], cuda_values_z[indexRing2], cudaRscanner, &P1, &P2);
+    LOR.x = P2.x - P1.x;
+    LOR.y = P2.y - P1.y;
+    LOR.z = P2.z - P1.z;
+    CUDA_Siddon (&LOR, &P1, volume, michelogram, PROJECTION, indiceMichelogram);
+  }	
+}
