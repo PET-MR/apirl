fac0a02918726ec1d12647822e80e8420edb4b19
belzunce <belzunce@4aa1ec02-d693-4363-b663-c1bee9069998>
2015-01-20 17:34:59 +0000

    First versions of scripts to be used on normalization.
    git-svn-id: https://svn.code.sf.net/p/apirl/code/trunk@41 4aa1ec02-d693-4363-b663-c1bee9069998

---
+++ matlab/imageRecon/normalization/createNormalizationPhantomACFs.m
@@ -0,0 +1,60 @@
+%  *********************************************************************
+%  Reconstruction Framework for Siemens Biograph mMR.  
+%  Autor: Martín Belzunce. Kings College London.
+%  Fecha de Creación: 19/01/2015
+%  *********************************************************************
+%  This scripts creates a image of the Ge-68 normalization phantom and
+%  write its in interfile format. It also generates a configuration file to
+%  generate the ACFs for different types of sinograms with APIRL.
+close all
+clear all
+
+%% PATHS FOR EXTERNAL FUNCTIONS
+addpath('/workspaces/Martin/KCL/Biograph_mMr/mmr');
+apirlPath = '/workspaces/Martin/PET/apirl-code/trunk/';
+addpath(genpath([apirlPath '/matlab']));
+
+%% OUTPUT PATH
+% Outputh were images and other files are writen:
+outputPath = '/workspaces/Martin/KCL/Biograph_mMr/mmr/5hr_ge68/';
+%% INICIALIZACIÓN IMÁGENES
+% Image of the Full Fov of the Siemens mMr
+sizeImage_mm = [594 594 258];
+sizeImage_pixels = [344 344 127];
+% Pixel size:
+sizePixel_mm = sizeImage_mm ./ sizeImage_pixels;
+imageAtenuation = zeros(sizeImage_pixels);
+mu_mass_aire = 8.712E-02;
+densidad_aire = 1.205E-03;
+mu_lineal_aire_1_cm = mu_mass_aire *densidad_aire;
+imageAtenuation(:) = mu_lineal_aire_1_cm;
+%% COORDINATE SYSTEM
+% El x vanza como los índices, osea a la izquierda es menor, a la derecha
+% mayor.
+coordX = -((sizeImage_mm(2)/2)-sizePixel_mm(1)/2):sizePixel_mm(1):((sizeImage_mm(2)/2)-sizePixel_mm(1)/2);
+% El y y el z van al revés que los índices, o sea el valor geométrico va a
+% contramano con los índices de las matrices.
+coordY = -((sizeImage_mm(1)/2)-sizePixel_mm(2)/2):sizePixel_mm(2):((sizeImage_mm(1)/2)-sizePixel_mm(2)/2);
+coordZ = -((sizeImage_mm(3)/2)-sizePixel_mm(3)/2):sizePixel_mm(3):((sizeImage_mm(3)/2)-sizePixel_mm(3)/2);
+[X,Y,Z] = meshgrid(coordX, coordY, coordZ);
+
+%% PHANTOM
+% Linear attenuation coefficent of Ge-68 resine:
+% Net weight of epoxy/Ge68 matrix
+% 24.0 lbs (10.9 kg)
+% Approximate volume of epoxy/Ge68 matrix
+% 10.9 kg / (1.058 g/cc) = 10.3 L
+% Mass attenuation coefficient of active resin@511 keV
+% 0.103 (cm2/g)
+mu_ge_resine_1_mm = 0.103 * 1.058 / 10;
+% Size of the phantom
+radiusGeCylinder_mm = 85;
+heightGeCylinder_mm = 275;
+indexGeCylinder = (sqrt((X-0).^2+(Y-0).^2) < radiusGeCylinder_mm)  & (Z>-(heightGeCylinder_mm/2))&(Z<(heightGeCylinder_mm/2));
+imageAtenuation(indexGeCylinder) = mu_ge_resine_1_mm;
+%% VISUALIZATION
+image = getImageFromSlices(imageAtenuation, 12, 1, 0);
+figure;
+imshow(image);
+%% ESCRITURA DE IMÁGENES EN INTERFILE
+interfilewrite(single(imageAtenuation), sprintf('%s/Ge60NormalizationPhantom',outputPath), sizePixel_mm);
---
+++ matlab/imageRecon/normalization/readmMrComponentBasedNormalization.m
@@ -0,0 +1,151 @@
+%  *********************************************************************
+%  Reconstruction Framework for Siemens Biograph mMR.  
+%  Autor: Martín Belzunce. Kings College London.
+%  Fecha de Creación: 15/01/2015
+%  *********************************************************************
+%  function [componentFactors, componentLabels]  = readmMrComponentBasedNormalization(filenameRawData, visualize)
+% 
+%  This functions read the component based normalization file of the mMr.
+%  Receives two parameters:
+%   -filenameRawData: the name of the file with the raw data(*.n).
+%   -visualize: flag to indicate if a visualization of each factor is
+%   desired.
+%
+%  The ouput is two cell arrays:
+%   -componentFactors: each element of this cell array is a matrix or
+%   vector with one of the component of the normalization.
+%   -componentLabels: cell array of the same size of componentFactors,
+%   where each of the elements is the name of the component. 
+% 
+%  The size of each component matrix are hardcoded for the mMr scanner and
+%  are taken from the following specification of the interfile header:
+%
+% %NORMALIZATION COMPONENTS DESCRIPTION:=
+% 
+% %number of normalization components:=8
+% %normalization component [1]:=geometric effects
+% %normalization component [2]:=crystal interference
+% %normalization component [3]:=crystal efficiencies
+% %normalization component [4]:=axial effects
+% %normalization component [5]:=paralyzing ring DT parameters
+% %normalization component [6]:=non-paralyzing ring DT parameters
+% %normalization component [7]:=TX crystal DT parameter
+% %normalization component [8]:=additional axial effects
+% 
+% data offset in bytes [1]:=0
+% data offset in bytes [2]:=174752
+% data offset in bytes [3]:=187136
+% data offset in bytes [4]:=316160
+% data offset in bytes [5]:=319508
+% data offset in bytes [6]:=319764
+% data offset in bytes [7]:=320020
+% data offset in bytes [8]:=320056
+% 
+% number of dimensions [1]:=2
+% number of dimensions [2]:=2
+% number of dimensions [3]:=2
+% number of dimensions [4]:=1
+% number of dimensions [5]:=1
+% number of dimensions [6]:=1
+% number of dimensions [7]:=1
+% number of dimensions [8]:=1
+% 
+% %matrix size [1]:={344,127}
+% %matrix size [2]:={9,344}
+% %matrix size [3]:={504,64}
+% %matrix size [4]:={837}
+% %matrix size [5]:={64}
+% %matrix size [6]:={64}
+% %matrix size [7]:={9}
+% %matrix size [8]:={837}
+% 
+% %matrix axis label [1]:={sinogram projection bins,sinogram planes}
+% %matrix axis label [2]:={crystal number,sinogram projection bins}
+% %matrix axis label [3]:={crystal number,ring number}
+% %matrix axis label [4]:={plane number}
+% %matrix axis label [5]:={ring number}
+% %matrix axis label [6]:={ring number}
+% %matrix axis label [7]:={crystal number}
+% %matrix axis label [8]:={plane number}
+% 
+% %matrix axis unit [1]:={mm/pixel,mm/pixel}
+% %matrix axis unit [2]:={mm/pixel,mm/pixel}
+% %matrix axis unit [3]:={mm/pixel,mm/pixel}
+% %matrix axis unit [4]:={mm/pixel}
+% %matrix axis unit [5]:={mm/pixel}
+% %matrix axis unit [6]:={mm/pixel}
+% %matrix axis unit [7]:={mm/pixel}
+% %matrix axis unit [8]:={mm/pixel}
+% 
+% %scale factor [1]:={2.0445,2.03125}
+% %scale factor [2]:={2.0445,2.0445}
+% %scale factor [3]:={2.0445,4.0625}
+% %scale factor [4]:={2.03125}
+% %scale factor [5]:={4.0625}
+% %scale factor [6]:={4.0625}
+% %scale factor [7]:={2.03125}
+% %scale factor [8]:={2.03125}
+% 
+% %axial compression:=11
+% %maximum ring difference:=60
+% number of rings:=64
+% number of energy windows:=1
+% %energy window lower level (keV) [1]:=430
+% %energy window upper level (keV) [1]:=610
+
+function [componentFactors, componentLabels]  = readmMrComponentBasedNormalization(filenameRawData, visualize)
+
+% Open file:
+fid = fopen(filenameRawData, 'r');
+if fid == -1
+    ferror(fid);
+end
+% Read all the files from the specification, the sizes are fixed but it
+% would be better to take the size fro the interfile header:
+geomEffects = fread(fid,[344 127], 'single=>single');
+crystalInterf = fread(fid,[9 344], 'single=>single');
+crystalEff = fread(fid,[504 64], 'single=>single');
+axialEffects = fread(fid,[837], 'single=>single');
+parRingDT = fread(fid,[64], 'single=>single'); % Paralyzing Death Time per Ring.
+nonParRingDT = fread(fid,[64], 'single=>single');  % Non-Paralyzing Death Time per Ring.
+txCrystalDT = fread(fid,[9], 'single=>single');
+otherAxialEffects = fread(fid,[837], 'single=>single');
+fclose(fid);
+
+componentFactors = {geomEffects, crystalInterf, crystalEff, axialEffects, parRingDT, nonParRingDT, txCrystalDT, otherAxialEffects};
+componentLabels = {'Geometric Effects','Crystal Interference','Crystal Efficencies','Axial Effects','Paralyzing Dead Time','non-Paralyzing Dead Time','Transmission Dead Time','Other Axial Effects'};
+
+% If the visualization flag is activated, show the factors:
+if visualize==1
+    % Plot the normalization parameters:
+    h = figure;
+    showImageWithColorbar(geomEffects, colormap);
+    title(componentLabels{1});
+    
+    h = figure;
+    subplot(1,2,1);
+    imshow(crystalInterf'./max(max(crystalInterf)));
+    title(componentLabels{2});
+    subplot(1,2,2);
+    imshow(crystalEff./max(max(crystalEff)));
+    title(componentLabels{3});
+
+    h = figure;
+    subplot(1,2,1);
+    plot(axialEffects);
+    title(componentLabels{4});
+    subplot(1,2,2);
+    plot(otherAxialEffects);
+    title(componentLabels{8});
+
+    h = figure;
+    subplot(1,3,1);
+    plot(parRingDT);
+    title(componentLabels{5});
+    subplot(1,3,2);
+    plot(nonParRingDT);
+    title(componentLabels{6});
+    subplot(1,3,3);
+    plot(txCrystalDT);
+    title(componentLabels{7});
+end
