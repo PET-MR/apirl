9b9f00a3284792aa35da54325446372e4fb55344
belzunce <belzunce@4aa1ec02-d693-4363-b663-c1bee9069998>
2015-04-16 15:37:23 +0000

    Script to creat system matrix for detector efficiencies ready.
    git-svn-id: https://svn.code.sf.net/p/apirl/code/trunk@87 4aa1ec02-d693-4363-b663-c1bee9069998

--- matlab/imageRecon/siemens_mmr/createDetectorSystemMatrix3d.m
+++ matlab/imageRecon/siemens_mmr/createDetectorSystemMatrix3d.m
@@ -8,7 +8,12 @@
 %  case.
 clear all
 close all
-
+%% PATHS FOR EXTERNAL FUNCTIONS AND RESULTS
+addpath('/home/mab15/workspace/KCL/Biograph_mMr/mmr');
+apirlPath = '/home/mab15/workspace/apirl-code/trunk/';
+addpath(genpath([apirlPath '/matlab']));
+setenv('PATH', [getenv('PATH') ':/home/mab15/workspace/apirl-code/trunk/build/bin']);
+setenv('LD_LIBRARY_PATH', [getenv('LD_LIBRARY_PATH') ':/home/mab15/workspace/apirl-code/trunk/build/bin']);
 %% GET THE NORM FACTORS TO GET EFFINCENCIES
 normFile = '/home/mab15/workspace/STIR/KCL/STIR_mMR_KCL/IM/NORM.n';
 componentFactors = readmMrComponentBasedNormalization(normFile, 0);
@@ -37,70 +42,71 @@ maxNumNonZeros = numBins; % One detector per bin.
 %     detector1SystemMatrix(:,i) = mapaDet1Ids(:) == i;
 %     detector2SystemMatrix(:,i) = mapaDet2Ids(:) == i;
 % end
-detector1SystemMatrix = sparse(1:numBins, mapaDet1Ids(:), true, numBins,numDetectors,maxNumNonZeros);
-detector2SystemMatrix = sparse(1:numBins, mapaDet2Ids(:), true, numBins,numDetectors,maxNumNonZeros);
-save detector1SystemMatrix '/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector1SystemMatrixSpan1'
-save detector2SystemMatrix '/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector2SystemMatrixSpan1'
+% This is more fficient to compute it:
+detector1SystemMatrix = sparse(1:numBins, double(mapaDet1Ids(:)), true, numBins,numDetectors,maxNumNonZeros);
+detector2SystemMatrix = sparse(1:numBins, double(mapaDet2Ids(:)), true, numBins,numDetectors,maxNumNonZeros);
+save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector1SystemMatrixSpan1.mat', 'detector1SystemMatrix', '-v7.3')
+save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector2SystemMatrixSpan1.mat', 'detector2SystemMatrix', '-v7.3')
 % Generate sinograms with system matrix:
 sinoEfficienciesSystemMatrix = (detector1SystemMatrix * double(crystalEffAllRings)) .* (detector2SystemMatrix * double(crystalEffAllRings));
 sinoEfficienciesSystemMatrix = reshape(sinoEfficienciesSystemMatrix, [numR numTheta sum(structSizeSino3d.sinogramsPerSegment)]);
 % Generate with function:
-sinoEfficencies = createSinogram3dFromDetectorsEfficency(crystalEffRing1, structSizeSino3d, 1, 0);
+sinoEfficencies = createSinogram3dFromDetectorsEfficency(double(crystalEff), structSizeSino3d, 0);
 
-numDiff = sum(sum(sinoEfficienciesSystemMatrix ~= sinoEfficencies));
+numDiff = sum(sum(single(sinoEfficienciesSystemMatrix) ~= sinoEfficencies));
 
 disp(sprintf('Hay %d bins diferentes en los sinogramas.', numDiff));
 %% TRANSPOSE TO GET THE EFFICENCIES FROM THE SINOGRAM
-effDet1 = detector1SystemMatrix'*sinoEfficencies(:) ./ (sum(detector1SystemMatrix',2));
+effDet1 = detector1SystemMatrix'*double(sinoEfficencies(:)) ./ (sum(detector1SystemMatrix',2));
 effDet1(isnan(effDet1)) = 0;
-figure;
-plot(effDet1);
-title('Estimated efficencies from Tranpose of System Matrix 1');
-effDet2 = detector2SystemMatrix'*sinoEfficencies(:) ./ (sum(detector2SystemMatrix',2));
+% figure;
+% plot(effDet1);
+% title('Estimated efficencies from Tranpose of System Matrix 1');
+effDet2 = detector2SystemMatrix'*double(sinoEfficencies(:)) ./ (sum(detector2SystemMatrix',2));
 effDet2(isnan(effDet2)) = 0;
-figure;
-plot(effDet2);
-title('Estimated efficencies from Tranpose of System Matrix 2');
+% figure;
+% plot(effDet2);
+% title('Estimated efficencies from Tranpose of System Matrix 2');
 
 % Combination of both efficencies:
 combEff = effDet1;
 combEff(effDet1 == 0) = effDet2(effDet1 == 0);
 combEff((effDet1 == 0)&(effDet2 == 0)) = (effDet1((effDet1 == 0)&(effDet2 == 0)) + effDet2((effDet1 == 0)&(effDet2 == 0))) / 2;
-figure;
-plot(1:numDetectors, crystalEffRing1, 1:numDetectors, combEff);
+% figure;
+% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEff);
 
 % Need to normalize, the efficencies are always normalized to the media:
 meanEfficency = mean(combEff(combEff~=0))
 combEffNorm = combEff / meanEfficency;
-figure;
-plot(1:numDetectors, crystalEffRing1, 1:numDetectors, combEffNorm);
+% figure;
+% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEffNorm);
 
 %% TRANSPOSE TO GET THE EFFICENCIES FROM THE SINOGRAM
 % Method 2 average normalizing over the sum:
-effDet1 = detector1SystemMatrix'*sinoEfficencies(:);
-figure;
-plot(effDet1);
-title('Estimated efficencies from Tranpose of System Matrix 1');
-effDet2 = detector2SystemMatrix'*sinoEfficencies(:);
+effDet1 = detector1SystemMatrix'*double(sinoEfficencies(:));
+% figure;
+% plot(effDet1);
+% title('Estimated efficencies from Tranpose of System Matrix 1');
+effDet2 = detector2SystemMatrix'*double(sinoEfficencies(:));
 effDet2(isnan(effDet2)) = 0;
-figure;
-plot(effDet2);
-title('Estimated efficencies from Tranpose of System Matrix 2');
+% figure;
+% plot(effDet2);
+% title('Estimated efficencies from Tranpose of System Matrix 2');
 
 % Combination of both efficencies:
 combEff = effDet1 + effDet2;
 combinedNormalization =  sum(detector1SystemMatrix',2) + sum(detector2SystemMatrix',2);
-combEff = combEff / combinedNormalization;
-figure;
-plot(1:numDetectors, crystalEffRing1, 1:numDetectors, combEff);
+combEff = combEff ./ combinedNormalization;
+% figure;
+% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEff);
 
 % Need to normalize, the efficencies are always normalized to the media:
 meanEfficency = mean(combEff(combEff~=0))
-combEffNorm = combEff / meanEfficency;
-figure;
-plot(1:numDetectors, crystalEffRing1, 1:numDetectors, combEffNorm);
+combEffNorm = combEff ./ meanEfficency;
+% figure;
+% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEffNorm);
 %% MATRIX VERSION
 % Forward:
-forwardSino = diag(detector1SystemMatrix * crystalEffRing1)*(crystalEffRing1'*detector2SystemMatrix');
+forwardSino = diag(detector1SystemMatrix * crystalEffAllRings)*(crystalEffAllRings'*detector2SystemMatrix');
 %% MATRIX VERSION 2
-forwardSino2 = detector1SystemMatrix*(crystalEffRing1*crystalEffRing1');
+forwardSino2 = detector1SystemMatrix*(crystalEffAllRings*crystalEffAllRings');
