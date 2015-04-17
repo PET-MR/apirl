be51d2f42cc8d0b325eaf03680921981d6d141ef
belzunce <belzunce@4aa1ec02-d693-4363-b663-c1bee9069998>
2015-04-17 17:35:18 +0000

    Function with create id of detectors for 3d for span, but that is not valid because of the quadratic effect of efficiencies.
    git-svn-id: https://svn.code.sf.net/p/apirl/code/trunk@88 4aa1ec02-d693-4363-b663-c1bee9069998

--- matlab/imageRecon/siemens_mmr/createDetectorSystemMatrix3d.m
+++ matlab/imageRecon/siemens_mmr/createDetectorSystemMatrix3d.m
@@ -3,29 +3,13 @@
 %  Autor: Martín Belzunce. Kings College London.
 %  Fecha de Creación: 19/02/2015
 %  *********************************************************************
-%  Creates the system matrix for the mlem algorithm for crystal
+%  Function that creates the system matrix for the mlem algorithm for crystal
 %  efficiencies. Goes from detectors to sinograms. This one is for he 3d
 %  case.
-clear all
-close all
-%% PATHS FOR EXTERNAL FUNCTIONS AND RESULTS
-addpath('/home/mab15/workspace/KCL/Biograph_mMr/mmr');
-apirlPath = '/home/mab15/workspace/apirl-code/trunk/';
-addpath(genpath([apirlPath '/matlab']));
-setenv('PATH', [getenv('PATH') ':/home/mab15/workspace/apirl-code/trunk/build/bin']);
-setenv('LD_LIBRARY_PATH', [getenv('LD_LIBRARY_PATH') ':/home/mab15/workspace/apirl-code/trunk/build/bin']);
-%% GET THE NORM FACTORS TO GET EFFINCENCIES
-normFile = '/home/mab15/workspace/STIR/KCL/STIR_mMR_KCL/IM/NORM.n';
-componentFactors = readmMrComponentBasedNormalization(normFile, 0);
-crystalEff = componentFactors{3};
-% This would be to include the gaps:
-crystalEff(1:9:end,:) = 0;
-% Transform in it in a vector:
-crystalEffAllRings = crystalEff(:);
-
+function [detector1SystemMatrix, detector2SystemMatrix] = createDetectorSystemMatrix3d(span, normalize)
 %% SYSTEM MATRIZ FOR SPAN1 SINOGRAMS
 % Size of mMr Sinogram's
-numTheta = 252; numR = 344; numRings = 64; maxAbsRingDiff = 60; rFov_mm = 594/2; zFov_mm = 258; span = 1;
+numTheta = 252; numR = 344; numRings = 64; maxAbsRingDiff = 60; rFov_mm = 594/2; zFov_mm = 258;
 structSizeSino3d = getSizeSino3dFromSpan(numR, numTheta, numRings, rFov_mm, zFov_mm, span, maxAbsRingDiff);
 numDetectorsPerRing = 504;
 numDetectors = numDetectorsPerRing*numRings;
@@ -42,71 +26,86 @@ maxNumNonZeros = numBins; % One detector per bin.
 %     detector1SystemMatrix(:,i) = mapaDet1Ids(:) == i;
 %     detector2SystemMatrix(:,i) = mapaDet2Ids(:) == i;
 % end
-% This is more fficient to compute it:
-detector1SystemMatrix = sparse(1:numBins, double(mapaDet1Ids(:)), true, numBins,numDetectors,maxNumNonZeros);
-detector2SystemMatrix = sparse(1:numBins, double(mapaDet2Ids(:)), true, numBins,numDetectors,maxNumNonZeros);
-save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector1SystemMatrixSpan1.mat', 'detector1SystemMatrix', '-v7.3')
-save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector2SystemMatrixSpan1.mat', 'detector2SystemMatrix', '-v7.3')
-% Generate sinograms with system matrix:
-sinoEfficienciesSystemMatrix = (detector1SystemMatrix * double(crystalEffAllRings)) .* (detector2SystemMatrix * double(crystalEffAllRings));
-sinoEfficienciesSystemMatrix = reshape(sinoEfficienciesSystemMatrix, [numR numTheta sum(structSizeSino3d.sinogramsPerSegment)]);
-% Generate with function:
-sinoEfficencies = createSinogram3dFromDetectorsEfficency(double(crystalEff), structSizeSino3d, 0);
+if span == 1
+    % This is more fficient to compute it:
+    detector1SystemMatrix = sparse(1:numBins, double(mapaDet1Ids(:)), true, numBins,numDetectors,maxNumNonZeros);
+    detector2SystemMatrix = sparse(1:numBins, double(mapaDet2Ids(:)), true, numBins,numDetectors,maxNumNonZeros);
+else
+    numBins = numR*numTheta*sum(structSizeSino3d.sinogramsPerSegment);
+    accIndicesBin = [];
+    accDetector1Ids = [];
+    accDetector2Ids = [];
+    % Transform the det Ids map to michelogram (4d matrix):
+    structSizeSino3d_span1 = getSizeSino3dFromSpan(numR, numTheta, numRings, rFov_mm, zFov_mm, 1, maxAbsRingDiff);
+    michelogramDet1Ids = generateMichelogramFromSinogram3D(mapaDet1Ids, structSizeSino3d_span1);
+    michelogramDet2Ids = generateMichelogramFromSinogram3D(mapaDet2Ids, structSizeSino3d_span1);
+    clear mapaDet1Ids;
+    clear mapaDet2Ids;
+    % Now we start going through each possible sinogram, then get the rings of
+    % each sinogram and get the crystal efficency for that ring in det1 and the
+    % crystal effiencies for the sencdo ring with det2. When there is axial
+    % compression the efficency is computed as an average of each of them. For
+    % example an sinogram compressed from two different axial position:
+    % det1(ring1_a)*det2(ring2_a)+det1(ring1_b)*det2(ring2_b)
+    indiceSino = 1; % indice del sinogram 3D.
+    numCrystalsMashed = zeros(1,numBins);
+    for segment = 1 : structSizeSino3d.numSegments
+        % Por cada segmento, voy generando los sinogramas correspondientes y
+        % contándolos, debería coincidir con los sinogramas para ese segmento: 
+        numSinosThisSegment = 0;
+        % Recorro todos los z1 para ir rellenando
+        for z1 = 1 : (structSizeSino3d.numZ*2)
+            numSinosZ1inSegment = 0;   % Cantidad de sinogramas para z1 en este segmento
+            % Recorro completamente z2 desde y me quedo con los que están entre
+            % minRingDiff y maxRingDiff. Se podría hacer sin recorrer todo el
+            % sinograma pero se complica un poco.
+            z1_aux = z1;    % z1_aux la uso para recorrer.
+            for z2 = 1 : structSizeSino3d.numZ
+                % Ahora voy avanzando en los sinogramas correspondientes,
+                % disminuyendo z1 y aumentnado z2 hasta que la diferencia entre
+                % anillos llegue a maxRingDiff.
+                if ((z1_aux-z2)<=structSizeSino3d.maxRingDiff(segment))&&((z1_aux-z2)>=structSizeSino3d.minRingDiff(segment))
+                    % Me asguro que esté dentro del tamaño del michelograma:
+                    if(z1_aux>0)&&(z2>0)&&(z1_aux<=structSizeSino3d.numZ)&&(z2<=structSizeSino3d.numZ)
+                        numSinosZ1inSegment = numSinosZ1inSegment + 1;
+                        % Generate an sparse matrix for this sinogram. Need
+                        % the linear index in the sinogram (depends on
+                        % index sino:
+                        indicesBin = ((indiceSino-1)*numR*numTheta + 1) : indiceSino*numR*numTheta; 
+                        % Detector ids for this bin:               
+                        detector1Ids = michelogramDet1Ids(:,:,z1_aux,z2);
+                        detector2Ids = michelogramDet2Ids(:,:,z1_aux,z2);
+                        % Acumulate indexes:
+                        accIndicesBin = [accIndicesBin indicesBin];
+                        accDetector1Ids = [accDetector1Ids detector1Ids(:)'];
+                        accDetector2Ids = [accDetector2Ids detector2Ids(:)'];
+                        % Normalization factor for number of crystal used:
+                        numCrystalsMashed(indicesBin) = numCrystalsMashed(indicesBin) + 1;
+                    end
+                end
+                % Pase esta combinación de (z1,z2), paso a la próxima:
+                z1_aux = z1_aux - 1;
+            end
+            if(numSinosZ1inSegment>0)
+                % I average the efficencies dividing by the number of axial
+                % combinations used for this sino:
+                numSinosThisSegment = numSinosThisSegment + 1;
+                indiceSino = indiceSino + 1;
+            end
+        end    
+    end
+    % Create the sparse matrix:
+    detector1SystemMatrix = sparse(accIndicesBin, double(accDetector1Ids(:)), 1./numCrystalsMashed(accIndicesBin), numBins,numDetectors,maxNumNonZeros);
+    detector2SystemMatrix = sparse(accIndicesBin, double(accDetector2Ids(:)), 1./numCrystalsMashed(accIndicesBin), numBins,numDetectors,maxNumNonZeros);
+end
 
-numDiff = sum(sum(single(sinoEfficienciesSystemMatrix) ~= sinoEfficencies));
+combinedNormalization =  sum(detector1SystemMatrix,1) + sum(detector2SystemMatrix,1);
 
-disp(sprintf('Hay %d bins diferentes en los sinogramas.', numDiff));
-%% TRANSPOSE TO GET THE EFFICENCIES FROM THE SINOGRAM
-effDet1 = detector1SystemMatrix'*double(sinoEfficencies(:)) ./ (sum(detector1SystemMatrix',2));
-effDet1(isnan(effDet1)) = 0;
-% figure;
-% plot(effDet1);
-% title('Estimated efficencies from Tranpose of System Matrix 1');
-effDet2 = detector2SystemMatrix'*double(sinoEfficencies(:)) ./ (sum(detector2SystemMatrix',2));
-effDet2(isnan(effDet2)) = 0;
-% figure;
-% plot(effDet2);
-% title('Estimated efficencies from Tranpose of System Matrix 2');
+% Check if the matrix is required to be normalized (by default is normalized):
+if nargin == 1 || ((nargin==2)&&(normalize==1))
+    for i = 1 : size(detector1SystemMatrix,2)
+        detector1SystemMatrix(:,i) = detector1SystemMatrix(:,i)./combinedNormalization(i)';
+        detector2SystemMatrix(:,i) = detector2SystemMatrix(:,i)./combinedNormalization(i)';
+    end
+end
 
-% Combination of both efficencies:
-combEff = effDet1;
-combEff(effDet1 == 0) = effDet2(effDet1 == 0);
-combEff((effDet1 == 0)&(effDet2 == 0)) = (effDet1((effDet1 == 0)&(effDet2 == 0)) + effDet2((effDet1 == 0)&(effDet2 == 0))) / 2;
-% figure;
-% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEff);
-
-% Need to normalize, the efficencies are always normalized to the media:
-meanEfficency = mean(combEff(combEff~=0))
-combEffNorm = combEff / meanEfficency;
-% figure;
-% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEffNorm);
-
-%% TRANSPOSE TO GET THE EFFICENCIES FROM THE SINOGRAM
-% Method 2 average normalizing over the sum:
-effDet1 = detector1SystemMatrix'*double(sinoEfficencies(:));
-% figure;
-% plot(effDet1);
-% title('Estimated efficencies from Tranpose of System Matrix 1');
-effDet2 = detector2SystemMatrix'*double(sinoEfficencies(:));
-effDet2(isnan(effDet2)) = 0;
-% figure;
-% plot(effDet2);
-% title('Estimated efficencies from Tranpose of System Matrix 2');
-
-% Combination of both efficencies:
-combEff = effDet1 + effDet2;
-combinedNormalization =  sum(detector1SystemMatrix',2) + sum(detector2SystemMatrix',2);
-combEff = combEff ./ combinedNormalization;
-% figure;
-% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEff);
-
-% Need to normalize, the efficencies are always normalized to the media:
-meanEfficency = mean(combEff(combEff~=0))
-combEffNorm = combEff ./ meanEfficency;
-% figure;
-% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEffNorm);
-%% MATRIX VERSION
-% Forward:
-forwardSino = diag(detector1SystemMatrix * crystalEffAllRings)*(crystalEffAllRings'*detector2SystemMatrix');
-%% MATRIX VERSION 2
-forwardSino2 = detector1SystemMatrix*(crystalEffAllRings*crystalEffAllRings');
---
+++ matlab/imageRecon/siemens_mmr/testCreateDetectorSystemMatrix3d.m
@@ -0,0 +1,131 @@
+%  *********************************************************************
+%  Reconstruction Framework for Siemens Biograph mMR.  
+%  Autor: Martín Belzunce. Kings College London.
+%  Fecha de Creación: 19/02/2015
+%  *********************************************************************
+%  Creates the system matrix for the mlem algorithm for crystal
+%  efficiencies. Goes from detectors to sinograms. This one is for he 3d
+%  case.
+clear all
+close all
+%% PATHS FOR EXTERNAL FUNCTIONS AND RESULTS
+addpath('/home/mab15/workspace/KCL/Biograph_mMr/mmr');
+apirlPath = '/home/mab15/workspace/apirl-code/trunk/';
+addpath(genpath([apirlPath '/matlab']));
+setenv('PATH', [getenv('PATH') ':/home/mab15/workspace/apirl-code/trunk/build/bin']);
+setenv('LD_LIBRARY_PATH', [getenv('LD_LIBRARY_PATH') ':/home/mab15/workspace/apirl-code/trunk/build/bin']);
+%% GET THE NORM FACTORS TO GET EFFINCENCIES
+normFile = '/home/mab15/workspace/STIR/KCL/STIR_mMR_KCL/IM/NORM.n';
+componentFactors = readmMrComponentBasedNormalization(normFile, 0);
+crystalEff = componentFactors{3};
+% This would be to include the gaps:
+crystalEff(1:9:end,:) = 0;
+% Transform in it in a vector:
+crystalEffAllRings = crystalEff(:);
+
+%% SYSTEM MATRIX FOR SPAN1 SINOGRAMS
+numTheta = 252; numR = 344; numRings = 64; maxAbsRingDiff = 60; rFov_mm = 594/2; zFov_mm = 258; span = 1;
+structSizeSino3d = getSizeSino3dFromSpan(numR, numTheta, numRings, rFov_mm, zFov_mm, span, maxAbsRingDiff);
+[detector1SystemMatrix, detector2SystemMatrix] = createDetectorSystemMatrix3d(1, 0);
+save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector1SystemMatrixSpan1.mat', 'detector1SystemMatrix', '-v7.3')
+save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector2SystemMatrixSpan1.mat', 'detector2SystemMatrix', '-v7.3')
+% Generate sinograms with system matrix:
+sinoEfficienciesSystemMatrix = (detector1SystemMatrix * double(crystalEffAllRings)) .* (detector2SystemMatrix * double(crystalEffAllRings));
+sinoEfficienciesSystemMatrix = reshape(sinoEfficienciesSystemMatrix, [numR numTheta sum(structSizeSino3d.sinogramsPerSegment)]);
+% Generate with function:
+sinoEfficencies = createSinogram3dFromDetectorsEfficency(double(crystalEff), structSizeSino3d, 0);
+
+numDiff = sum(sum(sum(single(sinoEfficienciesSystemMatrix) ~= sinoEfficencies)));
+
+disp(sprintf('Hay %d bins diferentes en los sinogramas.', numDiff));
+% TRANSPOSE TO GET THE EFFICENCIES FROM THE SINOGRAM
+% Method 2 average normalizing over the sum:
+effDet1 = detector1SystemMatrix'*double(sinoEfficencies(:));
+% Self normalized matrixes:
+% detector1SystemMatrix_aux = double(detector1SystemMatrix);
+% for i = 1 : size(detector1SystemMatrix,2)
+%     detector1SystemMatrix_aux(:,i) = double(detector1SystemMatrix(:,i))./combinedNormalization(i)';
+% end
+% 
+% detector2SystemMatrix_aux = double(detector2SystemMatrix);
+% for i = 1 : size(detector2SystemMatrix,2)
+%     detector2SystemMatrix_aux(:,i) = double(detector2SystemMatrix(:,i))./combinedNormalization(i)';
+% end
+% figure;
+% plot(effDet1);
+% title('Estimated efficencies from Tranpose of System Matrix 1');
+effDet2 = detector2SystemMatrix'*double(sinoEfficencies(:));
+effDet2(isnan(effDet2)) = 0;
+% figure;
+% plot(effDet2);
+% title('Estimated efficencies from Tranpose of System Matrix 2');
+
+% Combination of both efficencies:
+combEff = effDet1 + effDet2;
+combinedNormalization =  sum(detector1SystemMatrix',2) + sum(detector2SystemMatrix',2);
+combEff = combEff ./ combinedNormalization;
+% figure;
+% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEff);
+
+% Need to normalize, the efficencies are always normalized to the media:
+meanEfficency = mean(combEff(combEff~=0));
+combEffNorm = combEff ./ meanEfficency;
+figure;
+numDetectors = 504*64;
+plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEffNorm);
+%% SPAN 11 SYSTEM MATRIX
+numTheta = 252; numR = 344; numRings = 64; maxAbsRingDiff = 60; rFov_mm = 594/2; zFov_mm = 258; span = 11;
+structSizeSino3d = getSizeSino3dFromSpan(numR, numTheta, numRings, rFov_mm, zFov_mm, span, maxAbsRingDiff);
+[detector1SystemMatrix, detector2SystemMatrix] = createDetectorSystemMatrix3d(11, 0);
+save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector1SystemMatrixSpan11.mat', 'detector1SystemMatrix', '-v7.3')
+save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector2SystemMatrixSpan11.mat', 'detector2SystemMatrix', '-v7.3')
+[detector1SystemMatrix_norm, detector2SystemMatrix_norm] = createDetectorSystemMatrix3d(11, 1);
+save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector1SystemMatrixSpan11_norm.mat', 'detector1SystemMatrix', '-v7.3')
+save('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector2SystemMatrixSpan11_norm.mat', 'detector2SystemMatrix', '-v7.3')
+detector1SystemMatrix = load('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector1SystemMatrixSpan11.mat');
+detector1SystemMatrix = detector1SystemMatrix.detector1SystemMatrix;
+detector2SystemMatrix = load('/home/mab15/workspace/KCL/xtal_efficiencies/AnalyzeCrystalEfficienciesImpact/detector2SystemMatrixSpan11.mat');
+detector2SystemMatrix = detector2SystemMatrix.detector2SystemMatrix;
+% Generate sinograms with system matrix:
+sinoEfficienciesSystemMatrix = (detector1SystemMatrix * double(crystalEffAllRings)) .* (detector2SystemMatrix * double(crystalEffAllRings));
+sinoEfficienciesSystemMatrix = reshape(sinoEfficienciesSystemMatrix, [numR numTheta sum(structSizeSino3d.sinogramsPerSegment)]);
+% Generate with function:
+sinoEfficencies = createSinogram3dFromDetectorsEfficency(double(crystalEff), structSizeSino3d, 0);
+
+numDiff = sum(sum(sum(single(sinoEfficienciesSystemMatrix) ~= sinoEfficencies)));
+
+disp(sprintf('Hay %d bins diferentes en los sinogramas.', numDiff));
+% TRANSPOSE TO GET THE EFFICENCIES FROM THE SINOGRAM
+% Method 2 average normalizing over the sum:
+effDet1 = detector1SystemMatrix'*double(sinoEfficencies(:));
+% Self normalized matrixes:
+% detector1SystemMatrix_aux = double(detector1SystemMatrix);
+% for i = 1 : size(detector1SystemMatrix,2)
+%     detector1SystemMatrix_aux(:,i) = double(detector1SystemMatrix(:,i))./combinedNormalization(i)';
+% end
+% 
+% detector2SystemMatrix_aux = double(detector2SystemMatrix);
+% for i = 1 : size(detector2SystemMatrix,2)
+%     detector2SystemMatrix_aux(:,i) = double(detector2SystemMatrix(:,i))./combinedNormalization(i)';
+% end
+% figure;
+% plot(effDet1);
+% title('Estimated efficencies from Tranpose of System Matrix 1');
+effDet2 = detector2SystemMatrix'*double(sinoEfficencies(:));
+effDet2(isnan(effDet2)) = 0;
+% figure;
+% plot(effDet2);
+% title('Estimated efficencies from Tranpose of System Matrix 2');
+
+% Combination of both efficencies:
+combEff = effDet1 + effDet2;
+combinedNormalization =  sum(detector1SystemMatrix',2) + sum(detector2SystemMatrix',2);
+combEff = combEff ./ combinedNormalization;
+% figure;
+% plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEff);
+
+% Need to normalize, the efficencies are always normalized to the media:
+meanEfficency = mean(combEff(combEff~=0));
+combEffNorm = combEff ./ meanEfficency;
+figure;
+plot(1:numDetectors, crystalEffAllRings, 1:numDetectors, combEffNorm);
