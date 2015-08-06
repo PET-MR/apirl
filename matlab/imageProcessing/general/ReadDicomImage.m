34bed2b69fcfb26368e61fd50f552c48b207c60e
belzunce <belzunce@4aa1ec02-d693-4363-b663-c1bee9069998>
2015-08-06 15:22:38 +0000

    modification in read dicom iamge.
    git-svn-id: https://svn.code.sf.net/p/apirl/code/trunk@151 4aa1ec02-d693-4363-b663-c1bee9069998

--- matlab/imageProcessing/general/ReadDicomImage.m
+++ matlab/imageProcessing/general/ReadDicomImage.m
@@ -43,8 +43,7 @@ if isfield(dicomInfo, 'ImagesInAcquisition')
 else
     numSlices = numel(files); % It would be better to use dicomInfo.ImagesInAcquisition.
 end
-% Poistion of the top-left pixel of the first slice:
-posTopLeftPixel_1 = dicomInfo.ImagePositionPatient;
+
 % Pixel spacing:
 pixelSpacing_mm = [dicomInfo.PixelSpacing(1) dicomInfo.PixelSpacing(2) dicomInfo.SliceThickness];
 % We suppose that each slice has the same ImageOrientationPatient:
@@ -57,11 +56,25 @@ if(dotProd > 1e-5)
 end
 
 [indexCol, indexRow] = meshgrid(double(0:0:dicomInfo.Width-1),double(dicomInfo.Height-1));
+
 for i = 1 : numSlices
     dicomInfo = dicominfo([path files(i).name]);
-    sliceCoordinates(i) = dicomInfo.SliceLocation;
-    image(:,:,i) = dicomread([path files(i).name]);
-    
+    slice = dicomread([path files(i).name]);
+    if isfield(dicomInfo, 'InstanceNumber') % If we have the number of slice use it.
+        sliceIndex = dicomInfo.InstanceNumber;
+    else
+        sliceIndex = i;
+    end
+    image(:,:,sliceIndex) = slice;
+    sliceCoordinates(sliceIndex) = dicomInfo.SliceLocation;
+    % If the last or first slice I need the coordinates:
+    if sliceIndex == numSlices
+        % Poistion of the top-left pixel of the last slice:
+        posTopLeftPixel_N = dicomInfo.ImagePositionPatient;
+    elseif sliceIndex == 1
+        % Poistion of the top-left pixel of the first slice:
+        posTopLeftPixel_1 = dicomInfo.ImagePositionPatient;
+    end
     % This is the full code to get the coordinates of each pixel. Now we
     % apply it in the affine matrix instead of getting each coordinate.
 %     xMap_mm(:,:,i) = dicomInfo.ImagePositionPatient(1) + dicomInfo.ImageOrientationPatient(1) * dicomInfo.PixelSpacing(1) .* indexCol + dicomInfo.ImageOrientationPatient(4) * dicomInfo.PixelSpacing(2) .* indexRow; 
@@ -87,8 +100,7 @@ end
 % % Re-arrange the image in incresing z coordinate.
 %     image = image(:,:,sortedIndexes);
 
-% Poistion of the top-left pixel of the last slice:
-posTopLeftPixel_N = dicomInfo.ImagePositionPatient;
+
 % Dir z:
 dirZ = (posTopLeftPixel_N - posTopLeftPixel_1) / (numSlices-1);
 
@@ -132,3 +144,6 @@ end
 % overwrite the affineMatrix used before because this is the correct
 % affineMatrix:
 affineMatrix = [dircosX.*pixelSpacing_mm(1) dircosY.*pixelSpacing_mm(2) dirZ posTopLeftPixel_1; 0 0 0 1];
+
+% Convert to single the image:
+image = single(image);
