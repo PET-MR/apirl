7365d0f233d2041593ea6e7c408da5fa1e0d30c3
mehran1363 <mehran1363@4aa1ec02-d693-4363-b663-c1bee9069998>
2016-05-30 18:40:36 +0000

    git-svn-id: https://svn.code.sf.net/p/apirl/code/trunk@241 4aa1ec02-d693-4363-b663-c1bee9069998

--- matlab/andrew_reader_lab_software_interface/@PETDataClass/PETDataClass.m
+++ matlab/andrew_reader_lab_software_interface/@PETDataClass/PETDataClass.m
@@ -53,7 +53,7 @@ classdef PETDataClass < handle
         isSinogram
         ScanDuration_sec
         FrameTimePoints % [0,300,400,300]
-        Version = '0.2.1'   % Versioning Major.Minor.Rev   
+        Version = '0.2.1'   % Versioning Major.Minor.Rev
     end
     
     methods
@@ -82,10 +82,10 @@ classdef PETDataClass < handle
             PETData.FrameDuration_sec           = 3600; %sec.
             PETData.SoftwarePaths.STIR          = '';
             PETData.SoftwarePaths.Apirl         = '';
-            PETData.SoftwarePaths.WinePath      = [getenv('HOME') '/.wine/drive_c/']; 
+            PETData.SoftwarePaths.WinePath      = [getenv('HOME') '/.wine/drive_c/'];
             PETData.DataType                    = '';
             
-            PETData.isListMode                  =[]; 
+            PETData.isListMode                  =[];
             PETData.isSinogram                   =[];
             PETData.ScanDuration_sec            =[];
             PETData.FrameTimePoints             = [0,300,400,300];
@@ -107,16 +107,16 @@ classdef PETDataClass < handle
                 PETData.SoftwarePaths.e7.HistogramReplay   = 'C:\Siemens\PET\bin.win64-VA20\HistogramReplay.exe';
                 PETData.SoftwarePaths.e7.JSRecon12 = 'cscript C:\JSRecon12\JSRecon12.js ';
             end
-
+            
             
             % If not any path, ask for it.
             if nargin == 0
-            % prompt a window to select the data path
+                % prompt a window to select the data path
                 PETData.DataPath.path  = uigetdir([],'Select the folder contaning the data');
                 PETData = PETDataClass(PETData.DataPath.path );
             else
                 % Path or struct received.
-            
+                
                 if isstruct(varargin{1})
                     % get fields from user's input
                     vfields = fieldnames(varargin{1});
@@ -129,7 +129,7 @@ classdef PETDataClass < handle
                     end
                     %check the consistancy of the PETData.SinogramSize and the
                     %Gantry.span and Gantry.maxRingDiff
-
+                    
                 elseif isdir(varargin{1})
                     
                     PETData.DataPath.path  = varargin{1};
@@ -148,13 +148,13 @@ classdef PETDataClass < handle
                             PETData = prompt_JSRecon12(PETData, PETData.DataPath.path);
                             fprintf('Done.\n');
                         otherwise %'list_mode_interfile', 'sinogram_uncompressed_interfile'
-                            % 
+                            %
                             PETData = read_histogram_interfiles(PETData, PETData.DataPath.path);
                     end
                     
                     
                 else
-
+                    
                 end
                 % Check for optional parameters:
                 if nargin == 2
@@ -175,9 +175,9 @@ classdef PETDataClass < handle
                                 end
                             end
                         end
-
+                        
                         %Update PETData.SinogramSize based on Gantry.span and Gantry.maxRingDiff
-                        init_sinogram_size(PETData, PETData.span, PETData.Gantry.nCrystalRings, PETData.Gantry.maxRingDiff); 
+                        init_sinogram_size(PETData, PETData.span, PETData.Gantry.nCrystalRings, PETData.Gantry.maxRingDiff);
                     end
                 end
             end
@@ -204,9 +204,9 @@ classdef PETDataClass < handle
                 [path, name, ext] = fileparts(listing(i).name);
                 if strcmpi(ext,'.IMA') || strcmpi(ext,'.PDT') || length(ext) > 5
                     j_d = j_d + 1;
-                elseif strcmpi(ext,'.mhdr') && ~isempty(strfind(name,'.s')) && isempty(strfind(name,'uncomp'))
+                elseif (strcmpi(ext,'.mhdr') || strcmpi(ext,'.hdr')) && ~isempty(strfind(name,'.s')) && isempty(strfind(name,'uncomp'))
                     j_si = j_si + 1;
-                elseif (strcmpi(ext,'.mhdr') && ~isempty(strfind(name,'.s'))) && ~isempty(strfind(name,'uncomp'))
+                elseif ((strcmpi(ext,'.mhdr') || strcmpi(ext,'.hdr')) && ~isempty(strfind(name,'.s'))) && ~isempty(strfind(name,'uncomp'))
                     j_sui = j_sui + 1;
                 elseif strcmpi(ext,'.hdr') && exist([path PETData.bar name '.l'],'file')
                     j_lmi = j_lmi +1;
@@ -220,19 +220,19 @@ classdef PETDataClass < handle
             if j_d && any([j_si,j_lmi,j_sui])
                 error('please seperate interfiles from dicom files');
             end
-                                        
-             if j_d                                                    
-                  PETData.DataType = 'dicom';                                                                   
-             else
-                 if j_sui 
-                     PETData.DataType = 'sinogram_uncompressed_interfile';
-                 elseif j_si
-                     PETData.DataType = 'sinogram_interfile';
-                 else
-                     PETData.DataType = 'list_mode_interfile';
-                 end 
+            
+            if j_d
+                PETData.DataType = 'dicom';
+            else
+                if j_sui
+                    PETData.DataType = 'sinogram_uncompressed_interfile';
+                elseif j_si
+                    PETData.DataType = 'sinogram_interfile';
+                else
+                    PETData.DataType = 'list_mode_interfile';
+                end
             end
-
+            
             type = PETData.DataType;
         end
         
@@ -258,7 +258,7 @@ classdef PETDataClass < handle
                 ' -u "' PETData.DataPath.umap(1).n '","' PETData.DataPath.hardware_umap(1).n '"' ...
                 ' -n "' PETData.DataPath.norm '" --os "' PETData.DataPath.scatters(1).n '" --rs --force -l 73,. -d "' PETData.DataPath.rawdata_sino(frame).n '"'];
             [status,message] = system(command);
-
+            
         end
         
         function status = uncompress_emission(PETData)
@@ -277,7 +277,7 @@ classdef PETDataClass < handle
             % Load_sinogram() loads it, otherwise it calls the relevent functions (based on MethodSinoData)
             % to generate the sinogram, waits until the sinogram is
             % generated and then loads it.
-
+            
             if (PETData.isSinogram) && (frame>PETData.isSinogram)% multiple sinograms in a folder
                 warning('The requested sinogram number exceeds the number of available sinograms, loading the last one (%d)\n', PETData.isSinogram);
                 frame =PETData.isSinogram;
@@ -295,7 +295,7 @@ classdef PETDataClass < handle
                 else % STIR version
                     
                 end
-
+                
             end
             
             switch sType
@@ -326,21 +326,21 @@ classdef PETDataClass < handle
                     clear scatter_3D scatter_2D
             end
         end
-
+        
         function data = read_sinograms(~,filename, SinoSize)
             fid=fopen(filename,'r');
             d = fread(fid,prod(SinoSize),'float');
             data = single(reshape(d,SinoSize));
             fclose(fid);
         end
-
+        
         function Scatter3D = iSSRB(PETData,Scatter2D)
             
             nPlanePerSeg = Planes_Seg(PETData);
             
             mo = cumsum(nPlanePerSeg)';
-            no =[[1;mo(1:end-1)+1],mo];            
-
+            no =[[1;mo(1:end-1)+1],mo];
+            
             Scatter3D = zeros(PETData.sinogram_size.matrixSize,'single');
             Scatter3D(:,:,no(1,1):no(1,2),:) = Scatter2D;
             
@@ -364,7 +364,7 @@ classdef PETDataClass < handle
             scatter_3D =  scatter_3D./NCF;
             scatter_3D(gaps)=0;
             
-            Trues = P - R;     
+            Trues = P - R;
             
             for i = 1: size(ACF2,3)
                 acf_i = ACF2(:,:,i);
@@ -387,62 +387,72 @@ classdef PETDataClass < handle
     methods (Access = public)
         
         function  P = Prompts(PETData,frame)
-            if nargin==1 && ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )),
-                frame =1;
-                fprintf('loading the first frame/number data....\n')
+            if nargin==1
+                if PETData.isSinogram==1
+                    frame =1;
+                elseif((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )),
+                    frame =1;
+                    fprintf('loading the first frame/number data....\n')
+                end
             end
             P = Load_sinogram(PETData,'prompts',frame);
         end
         
         function  R = Randoms(PETData,frame)
-            if nargin==1 && ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )),
+            if nargin==1 && (PETData.isSinogram==1 || ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )))
                 frame =1;
             end
             R = Load_sinogram(PETData,'randoms',frame);
         end
         
         function  S = Scatters(PETData,frame)
-            if nargin==1 && ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )),
+            if nargin==1 && (PETData.isSinogram==1 || ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )))
                 frame =1;
             end
             S = Load_sinogram(PETData,'scatters',frame);
         end
         
         function  ncf = NCF(PETData,frame)
-            if nargin==1 && ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )),
+            if nargin==1 && (PETData.isSinogram==1 || ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )))
                 frame =1;
             end
             ncf = Load_sinogram(PETData,'NCF',frame);
         end
         
         function  acf = ACF(PETData,frame)
-            if nargin==1 && ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )),
+            if nargin==1 && (PETData.isSinogram==1 || ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )))
                 frame =1;
             end
             acf = Load_sinogram(PETData,'ACF',frame);
         end
         
         function  acf = ACF2(PETData,frame)
-            if nargin==1 && ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )),
+            if nargin==1 && (PETData.isSinogram==1 || ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )))
                 frame =1;
             end
             acf = Load_sinogram(PETData,'ACF2',frame);
         end
         
         function mul = AN(PETData,frame)
-            mul = PETData.ACF(frame) .* PETData.NCF(frame);
+            if nargin==1 && (PETData.isSinogram==1 || ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )))
+                frame =1;
+            end
+            mul = PETData.ACF .* PETData.NCF(frame);
             mul = 1./mul;
             mul(isinf(mul)) = 0;
         end
         
         function add = RS(PETData,frame)
+            if nargin==1 && (PETData.isSinogram==1 || ((PETData.isSinogram)>1 || (PETData.isListMode && (PETData.NumberOfFrames>1) )))
+                frame =1;
+            end
             add = PETData.ACF(frame) .* PETData.NCF(frame).*(PETData.Scatters(frame) + PETData.Randoms(frame));
         end
         
-%         function mu = mumap(PETData)
-%             mu = Load_image();
-%         end
-
+        %         function mu = mumap(PETData)
+        %             mu = Load_image();
+        %         end
+        
         function info = get_acquisition_info(PETData)
             % get_acquisition_info() returns a structure contaning all key
             % infromation from dicom or interfile headers such injected dose,
@@ -470,8 +480,8 @@ classdef PETDataClass < handle
             PETData.NumberOfFrames              = length(PETData.FrameTimePoints)-1;
             
             if strcmpi(PETData.DataType,'dicom')
-            PETData.prompt_JSRecon12(PETData.DataPath.path,FrameName);
-            else 
+                PETData.prompt_JSRecon12(PETData.DataPath.path,FrameName);
+            else
                 PETData.read_histogram_interfiles(PETData.DataPath.path,FrameName);
             end
         end
@@ -487,15 +497,15 @@ classdef PETDataClass < handle
             % Read from the header of the list file, the total time:
             % Number of frames.
             PETData.NumberOfFrames = ceil(scanTime_sec./timeFrame_sec);
-
+            
             % List with the frames time stamps (it has NumberOfFrames elements):
             PETData.DynamicFrames_sec = zeros(PETData.NumberOfFrames,1);
-        
+            
             % Generate the time stamps for the frames:
             PETData.DynamicFrames_sec = 0 : timeFrame_sec : timeFrame_sec*PETData.NumberOfFrames;
         end
         
-
+        
         function SinogramDisplay(PETData,sType)
             if strcmpi(sType,'ACF')
                 temp = ACF(PETData,1);
@@ -518,7 +528,7 @@ classdef PETDataClass < handle
         end
         
         function PlotMichelogram(PETData)
-
+            
             Span = PETData.span;
             MRD = PETData.Gantry.maxRingDiff;
             nCrystalRings  = PETData.Gantry.nCrystalRings;
@@ -549,7 +559,7 @@ classdef PETDataClass < handle
             figure,imagesc(Michelogram),colormap gray, axis xy
             title(sprintf('No. Segments:%d, Span: %d, MRD: %d', nSeg, Span, MRD))
         end
-
+        
         
         function uncompress(PETData,Dir)
             if nargin==1
@@ -567,13 +577,13 @@ end
 
 
 %         function path = STIR_scatter_sino()
-%             
+%
 %         end
-%         
+%
 %         function path = Apirl_norm_factors()
-%             
+%
 %         end
-%         
+%
 %         function path = e7_norm_factors()
 %             % e7_norm_factors calls e7_norm to generate frame dependent norm factors in
 %             % the case of dynamic or framed data
@@ -584,13 +594,12 @@ end
 %             % converts Dicom to interfile, uses the interfile header
 %             % availabe in Dicom Private_0029_1010 field
 %         end
-%         
+%
 %         function make_mu_map(),end
-%         
+%
 %         function load_hardware_mu_map(), end
-%         
+%
 %         function Members(PETData)
 %             %displays members and defaults values
-% 
+%
 %         end
-        
