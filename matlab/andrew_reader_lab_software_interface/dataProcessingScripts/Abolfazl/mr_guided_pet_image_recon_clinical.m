ffb9de69310fdd2b01b663768a6d1a2401463888
mehran1363 <mehran1363@4aa1ec02-d693-4363-b663-c1bee9069998>
2017-04-24 16:58:20 +0000

    git-svn-id: https://svn.code.sf.net/p/apirl/code/trunk@336 4aa1ec02-d693-4363-b663-c1bee9069998

---
+++ matlab/andrew_reader_lab_software_interface/dataProcessingScripts/Abolfazl/mr_guided_pet_image_recon_clinical.m
@@ -0,0 +1,32 @@
+% load data
+load('\\bioeng202-pc\PET-M\AD_patients\P02\e7\data-Converted\data-LM-00\sino_rawdata_100\rawdata_matlab.mat', 'Prompts','RS','AN','T1');
+
+% initialize PET object
+set_framework_environment();
+
+opt.method =  'otf_siddon_gpu';
+opt.nSubsets = 1;
+opt.nIter = 100;
+opt.PSF.Width = 2.5;
+
+PET = classGpet(opt);
+SensImg = PET.Sensitivity(AN);
+
+%% MAP PET image reconstruction
+opt.OptimizationMethod = 'DePierro';%'OSL'
+opt.PriorType = 'Bowsher';%'Quadratic'; %'JointBurgEntropy', 
+opt.MrImage = T1./max(T1(:));
+opt.display = 40;
+% parameters experimentally chosen for \AD_patients\P02 data
+opt.BowsherB = 40;
+opt.MrSigma = 0.01;
+opt.PetSigma  = 1;
+
+if strcmpi(opt.PriorType,'Quadratic')
+    opt.RegualrizationParameter = 3.0e+03;
+else
+    opt.RegualrizationParameter = 2.0e+05;
+end
+
+Img = PET.MAPEM(Prompts,RS, SensImg,PET.ones,PET.nIter,opt);
+
